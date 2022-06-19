44245180e2b9827cd78e89b5a477c01b31618cbe
hw-lunemann <hw.lunemann@gmail.com>
2022-06-20 00:57:01 +0200

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,33 @@
-fn main() {
+use clap::Parser;
+use std::path::{Path, PathBuf};
+
+#[derive(Parser)]
+struct Cli {
+    #[clap(value_parser, display_order = 0)]
+    path: PathBuf,
+    #[clap(value_parser, possible_values(&["+", "-"]), display_order = 1)]
+    action: char,
+    #[clap(value_parser, display_order = 2)]
+    step: usize,
+}
+
+fn read_to_f32(path: &Path) -> Result<f32, Box<dyn std::error::Error>> {
+    let text = std::fs::read_to_string(&path)?;
+    let value = text.replace('\n', "").parse()?;
+    Ok(value)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = Cli::parse();
+    let factor = match cli.action {
+        '-' => 1.0 - cli.step as f32 / 100.0,
+        '+' => 1.0 + cli.step as f32 / 100.0,
+        _ => unreachable!(),
+    };
+    let brightness_file = &cli.path.join("brightness");
+    let brightness = read_to_f32(brightness_file)?;
+    let max_brightness = read_to_f32(&cli.path.join("max_brightness"))?;
+    let min = max_brightness.min(brightness * factor) as usize;
+    std::fs::write(brightness_file, min.to_string().as_bytes())?;
+    Ok(())
 }
