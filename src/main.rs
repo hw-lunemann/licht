b6490e208b6ea67753c50df258c427d3ae4fd80f
hw-lunemann <hw.lunemann@gmail.com>
2022-06-20 11:27:17 +0200

    add operation modes

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,63 @@
 use anyhow::Context;
-use clap::Parser;
+use clap::{Parser, ValueEnum, PossibleValue};
 use std::path::{Path, PathBuf};
 
 #[derive(Parser)]
 struct Cli {
     #[clap(value_parser, display_order = 0)]
     path: PathBuf,
-    #[clap(value_parser, possible_values(&["+", "-"]), display_order = 1)]
-    action: char,
+    #[clap(value_enum, display_order = 1)]
+    action: Action,
     #[clap(value_parser, display_order = 2)]
     step: usize,
+    #[clap(value_enum, long, default_value("relative"))]
+    mode: Mode
 }
+
+#[derive(clap::ValueEnum, Clone)]
+enum Mode {
+    CurrentRelative, MaxRelative
+}
+
+#[derive(Clone)]
+enum Action {
+    Plus, Minus
+}
+
+impl ValueEnum for Action {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Self::Plus, Self::Minus]
+    }
+
+    fn to_possible_value<'a>(&self) -> Option<clap::PossibleValue<'a>> {
+        match self {
+            Action::Plus => Some(PossibleValue::new("+")),
+            Action::Minus => Some(PossibleValue::new("-")),
+        }
+    }
+}
+
+fn calculate_brightness(action: Action, step: usize, mode: Mode, current: f32, max: f32) -> f32 {
+    match mode {
+        Mode::CurrentRelative => {
+            let step = step as f32 / 100.0f32;
+            let diff = current * step;
+            match action {
+                Action::Plus => current + diff,
+                Action::Minus => current - diff
+            }
+        },
+        Mode::MaxRelative => { 
+            let step = step as f32 / 100.0f32;
+            let diff = max * step;
+            match action {
+                Action::Plus => current + diff,
+                Action::Minus => current - diff
+            }
+        },
+    }
+}
+
 
 fn read_to_f32(path: &Path) -> anyhow::Result<f32> {
     let text = std::fs::read_to_string(&path)?;
@@ -25,15 +72,11 @@ fn main() -> anyhow::Result<()> {
     }
 
 
-    let factor = match cli.action {
-        '-' => 1.0 - cli.step as f32 / 100.0,
-        '+' => 1.0 + cli.step as f32 / 100.0,
-        _ => unreachable!(),
-    };
     let brightness_file = &cli.path.join("brightness");
     let brightness = read_to_f32(brightness_file)?;
     let max_brightness = read_to_f32(&cli.path.join("max_brightness"))?;
-    let min = max_brightness.min(brightness * factor) as usize;
+    let new_brigtness = calculate_brightness(cli.action, cli.step, cli.mode, brightness, max_brightness)
+        .min(max_brightness);
 
-    std::fs::write(brightness_file, min.to_string().as_bytes()).context("writing brightness failed")
+    std::fs::write(brightness_file, new_brigtness.to_string().as_bytes()).context("writing brightness failed")
 }
