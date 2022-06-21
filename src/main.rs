a49a58e81437af6052c360181e653270f9b8cd49
hw-lunemann <hw.lunemann@gmail.com>
2022-06-21 16:49:29 +0200

    rename mode to stepping

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ struct Cli {
     #[clap(value_parser, display_order = 2)]
     step: usize,
     #[clap(value_enum, long, default_value("max-relative"))]
-    mode: Mode,
+    stepping: Stepping,
     #[clap(value_parser, long, default_value("2"))]
     exponent: f32,
     #[clap(value_parser, long)]
@@ -19,7 +19,7 @@ struct Cli {
 }
 
 #[derive(Clone)]
-enum Mode {
+enum Stepping {
     Absolute,
     CurrentRelative,
     MaxRelative,
@@ -28,7 +28,7 @@ enum Mode {
     },
 }
 
-impl ValueEnum for Mode {
+impl ValueEnum for Stepping {
     fn value_variants<'a>() -> &'a [Self] {
         &[Self::Absolute, Self::CurrentRelative, Self::MaxRelative, Self::MaxRelativeQuadratic { exponent: 2.0f32 }]
     }
@@ -84,15 +84,15 @@ impl Backlight {
         self.brightness as f32 / self.max_brightness as f32
     }
 
-    fn change_brightness(&mut self, action: Action, step: usize, mode: Mode) -> anyhow::Result<()> {
+    fn change_brightness(&mut self, action: Action, step: usize, stepping: Stepping) -> anyhow::Result<()> {
         let brigthness = self.brightness as f32;
         let max_brightness = self.max_brightness as f32;
-        let new_brightness = match mode {
-            Mode::Absolute => match action {
+        let new_brightness = match stepping {
+            Stepping::Absolute => match action {
                 Action::Plus => brigthness + step as f32,
                 Action::Minus => brigthness - step as f32,
             },
-            Mode::CurrentRelative => {
+            Stepping::CurrentRelative => {
                 let step = step as f32 / 100.0f32;
                 let diff = brigthness * step;
                 match action {
@@ -100,7 +100,7 @@ impl Backlight {
                     Action::Minus => brigthness - diff,
                 }
             }
-            Mode::MaxRelative => {
+            Stepping::MaxRelative => {
                 let step = step as f32 / 100.0f32;
                 let diff = max_brightness * step;
                 match action {
@@ -108,7 +108,7 @@ impl Backlight {
                     Action::Minus => brigthness - diff,
                 }
             }
-            Mode::MaxRelativeQuadratic { exponent } => {
+            Stepping::MaxRelativeQuadratic { exponent } => {
                 let cur_x = (brigthness / max_brightness).powf(1.0f32/exponent);
                 let new_x = match action {
                     Action::Plus => cur_x + (step as f32 / 100.0f32),
@@ -132,15 +132,15 @@ fn read_to_usize<P: AsRef<Path>>(path: P) -> anyhow::Result<usize> {
 
 fn main() -> anyhow::Result<()> {
     let mut cli = Cli::parse();
-    if let Mode::MaxRelativeQuadratic { .. } = cli.mode {
-        cli.mode = Mode::MaxRelativeQuadratic { exponent: cli.exponent };
+    if let Stepping::MaxRelativeQuadratic { .. } = cli.stepping {
+        cli.stepping = Stepping::MaxRelativeQuadratic { exponent: cli.exponent };
     }
 
     let mut backlight = Backlight::new(&cli.device)?;
     if cli.verbose {
         print!("{} -> ", &backlight.get_percent());
     }
-    backlight.change_brightness(cli.action, cli.step, cli.mode)?;
+    backlight.change_brightness(cli.action, cli.step, cli.stepping)?;
     if cli.verbose {
         print!("{}", &backlight.get_percent());
     }
