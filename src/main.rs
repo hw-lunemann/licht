87c3eabaa51b5115c50c22493c0161326ef248f3
hw-lunemann <hw.lunemann@gmail.com>
2022-06-21 16:48:02 +0200

    introduce backlight struct

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,42 @@ use std::path::{Path, PathBuf};
 #[derive(Parser)]
 struct Cli {
     #[clap(value_parser, display_order = 0)]
-    path: PathBuf,
+    device: String,
     #[clap(value_enum, display_order = 1)]
     action: Action,
     #[clap(value_parser, display_order = 2)]
     step: usize,
     #[clap(value_enum, long, default_value("max-relative"))]
     mode: Mode,
+    #[clap(value_parser, long, default_value("2"))]
+    exponent: f32,
     #[clap(value_parser, long)]
     verbose: bool
 }
 
-#[derive(clap::ValueEnum, Clone)]
+#[derive(Clone)]
 enum Mode {
     Absolute,
     CurrentRelative,
     MaxRelative,
-    MaxRelativeQuadratic,
+    MaxRelativeQuadratic { 
+        exponent: f32
+    },
+}
+
+impl ValueEnum for Mode {
+    fn value_variants<'a>() -> &'a [Self] {
+        &[Self::Absolute, Self::CurrentRelative, Self::MaxRelative, Self::MaxRelativeQuadratic { exponent: 2.0f32 }]
+    }
+
+    fn to_possible_value<'a>(&self) -> Option<PossibleValue<'a>> {
+        match self {
+            Self::Absolute => Some(PossibleValue::new("absolute")),
+            Self::CurrentRelative => Some(PossibleValue::new("current-relative")),
+            Self::MaxRelative => Some(PossibleValue::new("max-relative")),
+            Self::MaxRelativeQuadratic { .. } => Some(PossibleValue::new("max-relative-quadratic"))
+        }
+    }
 }
 
 #[derive(Clone)]
@@ -37,67 +56,94 @@ impl ValueEnum for Action {
 
     fn to_possible_value<'a>(&self) -> Option<clap::PossibleValue<'a>> {
         match self {
-            Action::Plus => Some(PossibleValue::new("+")),
-            Action::Minus => Some(PossibleValue::new("-")),
+            Self::Plus => Some(PossibleValue::new("+")),
+            Self::Minus => Some(PossibleValue::new("-")),
         }
     }
 }
 
-fn calculate_brightness(action: Action, step: usize, mode: Mode, current: f32, max: f32) -> f32 {
-    match mode {
-        Mode::Absolute => match action {
-            Action::Plus => current + step as f32,
-            Action::Minus => current - step as f32,
-        },
-        Mode::CurrentRelative => {
-            let step = step as f32 / 100.0f32;
-            let diff = current * step;
-            match action {
-                Action::Plus => current + diff,
-                Action::Minus => current - diff,
+struct Backlight {
+    brightness: usize,
+    brightness_path: PathBuf,
+    max_brightness: usize,
+}
+
+impl Backlight {
+    fn new(name: &str) -> anyhow::Result<Self> {
+        let device_path = Path::new("/sys/class/backlight/").join(name);
+        let brightness_path = device_path.join("brightness");
+
+        Ok(Self {
+            brightness: read_to_usize(&brightness_path)?,
+            max_brightness: read_to_usize(device_path.join("max_brightness"))?,
+            brightness_path: device_path.join(brightness_path)
+        })
+    }
+
+    fn get_percent(&self) -> f32 {
+        self.brightness as f32 / self.max_brightness as f32
+    }
+
+    fn change_brightness(&mut self, action: Action, step: usize, mode: Mode) -> anyhow::Result<()> {
+        let brigthness = self.brightness as f32;
+        let max_brightness = self.max_brightness as f32;
+        let new_brightness = match mode {
+            Mode::Absolute => match action {
+                Action::Plus => brigthness + step as f32,
+                Action::Minus => brigthness - step as f32,
+            },
+            Mode::CurrentRelative => {
+                let step = step as f32 / 100.0f32;
+                let diff = brigthness * step;
+                match action {
+                    Action::Plus => brigthness + diff,
+                    Action::Minus => brigthness - diff,
+                }
             }
-        }
-        Mode::MaxRelative => {
-            let step = step as f32 / 100.0f32;
-            let diff = max * step;
-            match action {
-                Action::Plus => current + diff,
-                Action::Minus => current - diff,
+            Mode::MaxRelative => {
+                let step = step as f32 / 100.0f32;
+                let diff = max_brightness * step;
+                match action {
+                    Action::Plus => brigthness + diff,
+                    Action::Minus => brigthness - diff,
+                }
             }
-        }
-        Mode::MaxRelativeQuadratic => {
-            let cur_x = (current / max).sqrt();
-            let new_x = match action {
-                Action::Plus => cur_x + (step as f32 / 100.0f32),
-                Action::Minus => cur_x - (step as f32 / 100.0f32),
-            };
-            max * new_x.powi(2)
-        }
+            Mode::MaxRelativeQuadratic { exponent } => {
+                let cur_x = (brigthness / max_brightness).powf(1.0f32/exponent);
+                let new_x = match action {
+                    Action::Plus => cur_x + (step as f32 / 100.0f32),
+                    Action::Minus => cur_x - (step as f32 / 100.0f32),
+                };
+                max_brightness * new_x.powf(exponent)
+            }
+        };
+
+        self.brightness = max_brightness.min(new_brightness) as usize;
+
+        std::fs::write(&self.brightness_path, &self.brightness.to_string().as_bytes())
+            .context("writing brightness failed")
     }
 }
 
-fn read_to_f32(path: &Path) -> anyhow::Result<f32> {
+fn read_to_usize<P: AsRef<Path>>(path: P) -> anyhow::Result<usize> {
     let text = std::fs::read_to_string(&path)?;
     text.replace('\n', "").parse().context("parse failure")
 }
 
 fn main() -> anyhow::Result<()> {
-    let cli = Cli::parse();
-
-    if !cli.path.starts_with("/sys/class/backlight/") {
-        anyhow::bail!("input had to be from the backlight device class in sysfs.")
+    let mut cli = Cli::parse();
+    if let Mode::MaxRelativeQuadratic { .. } = cli.mode {
+        cli.mode = Mode::MaxRelativeQuadratic { exponent: cli.exponent };
     }
 
-    let brightness_file = &cli.path.join("brightness");
-    let brightness = read_to_f32(brightness_file)?;
-    let max_brightness = read_to_f32(&cli.path.join("max_brightness"))?;
-    let new_brightness =
-        calculate_brightness(cli.action, cli.step, cli.mode, brightness, max_brightness)
-            .min(max_brightness);
-
+    let mut backlight = Backlight::new(&cli.device)?;
     if cli.verbose {
-        println!("{} -> {}", brightness/max_brightness, new_brightness/max_brightness);
+        print!("{} -> ", &backlight.get_percent());
     }
-    std::fs::write(brightness_file, (new_brightness as usize).to_string().as_bytes())
-        .context("writing brightness failed")
+    backlight.change_brightness(cli.action, cli.step, cli.mode)?;
+    if cli.verbose {
+        print!("{}", &backlight.get_percent());
+    }
+
+    Ok(())
 }
